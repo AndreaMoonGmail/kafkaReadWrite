17a77d61a781252092210ea4168a0e47a9822792
Andrea Sartirana <andrea.sartirana@mooney.it>
2025-01-12 15:52:53 +0100

    initial commit

---
+++ src/main/java/com/kafka/kafkaReadWrite/KafkaReadWriteApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkaReadWrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReadWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReadWriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/kafkaReadWrite/KafkaReadWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkaReadWrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaReadWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
